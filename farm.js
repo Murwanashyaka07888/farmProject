4e48ce636e3068f6112f7237ab80266319d73dc6
Murwanashyaka07888 <murwanashyakaclaude11@gmail.com>
2023-11-10 14:20:52 +0200

    other new code added

---
+++ farm.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+var swiper = new Swiper(".swiper",{
+ loop: true,
+ centeredSlides:'true',
+ slidesPerview: 3,
+ spaceBetween: 20,
+    pagnation: {
+        el: ".swiper-pagnation",
+        clickable: true,
+        type: 'bullets',
+    },
+})
